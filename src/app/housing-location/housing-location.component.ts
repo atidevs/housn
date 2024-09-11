1aa3d70bc055b62a89352b3b7d351611b61a0f61
Atmane TA <42352387+atidevs@users.noreply.github.com>
2024-09-11 12:13:15 +0200

    [feat]: implement housing details page and router based navigation

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
